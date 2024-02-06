bd6f8bd7ee52e5ce061be09622eb149d31879ef6
NathanPERIER <periernathan2000@gmail.com>
2024-02-06 17:10:56 +0100

    Added better argument parsing and "whiteboard" option

--- floyd-steinberg.py
+++ floyd-steinberg.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+from collections import deque
 from PIL import Image
 
 from src.floyd_steinberg import floyd_steinberg
@@ -11,12 +12,14 @@ def help() :
 
 class FloydSteinbergCallback :
 
-    def __init__(self, width: int, height: int) :
+    def __init__(self, width: int, height: int, whiteboard: bool) :
         self.img = Image.new(mode="RGB", size=(width, height))
         self.pixels = self.img.load()
+        self.mapper = (lambda v: 255 - v*255) if whiteboard else (lambda v: v*255)
     
     def __call__(self, x: int, y: int, v: int) :
-        self.pixels[x,y] = (v*255, v*255, v*255)
+        g = self.mapper(v)
+        self.pixels[x,y] = (g, g, g)
     
     def get_image(self) -> Image.Image :
         return self.img
@@ -24,19 +27,26 @@ class FloydSteinbergCallback :
 
 def main() :
 
-    if len(sys.argv) > 1 and sys.argv[0] in ['-h', '--help'] :
+    args = deque(sys.argv[1:])
+
+    if len(args) > 1 and args[0] in ['-h', '--help'] :
         help()
         sys.exit(0)
     
-    if len(sys.argv) < 3 :
+    whiteboard = False
+    if len(args) >= 1 and args[0] in ['--whiteboard', '--wb'] :
+        whiteboard = True
+        args.popleft()
+    
+    if len(args) < 2 :
         help()
         sys.exit(1)
     
-    src_image_path = sys.argv[1]
-    dst_image_path = sys.argv[2]
+    src_image_path = args[0]
+    dst_image_path = args[1]
 
     img = Image.open(src_image_path)
-    callback = FloydSteinbergCallback(img.width, img.height)
+    callback = FloydSteinbergCallback(img.width, img.height, whiteboard)
     floyd_steinberg(img, callback)
     callback.get_image().save(dst_image_path)
 
--- stegano.py
+++ stegano.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+from collections import deque
 from PIL import Image
 from tqdm import tqdm
 
@@ -23,23 +24,45 @@ def constrain_max_size(img: Image.Image, width: int, height: int) -> Image.Image
     return img
 
 
-def apply_lsb(value: int, lsb: int) :
+def apply_lsb_approx(value: int, lsb: int) :
+    """
+    Sets the bit as a mathematical approximation of the b/w image
+    (i.e. 0 is black, 1 is white)
+    """
     return value & 254 if lsb == 0 else value | 1
 
-pixel_lsb_mappers: dict[str,Callable[[Tuple[int,int,int],int],Tuple[int,int,int]]] = {
-    'r': (lambda pixel, lsb: (apply_lsb(pixel[0], lsb), pixel[1], pixel[2])),
-    'g': (lambda pixel, lsb: (pixel[0], apply_lsb(pixel[1], lsb), pixel[2])),
-    'b': (lambda pixel, lsb: (pixel[0], pixel[1], apply_lsb(pixel[2], lsb)))
-}
+def apply_lsb_whiteboard(value: int, lsb: int) :
+    """
+    Sets the bit as if writing with a marker on a whitebaord
+    (i.e. 0 is white, 1 is black)
+    """
+    return value | 1 if lsb == 0 else value & 254
+
+def get_lsb_mapper(channel: Literal['r', 'g', 'b'], whiteboard: bool) -> Callable[[Tuple[int,int,int],int],Tuple[int,int,int]] :
+    apply_lsb: Callable[[int,int],int] = apply_lsb_whiteboard if whiteboard else apply_lsb_approx
+    pixel_lsb_mappers: dict[str,Callable[[Tuple[int,int,int],int],Tuple[int,int,int]]] = {
+        'r': (lambda pixel, lsb: (apply_lsb(pixel[0], lsb), pixel[1], pixel[2])),
+        'g': (lambda pixel, lsb: (pixel[0], apply_lsb(pixel[1], lsb), pixel[2])),
+        'b': (lambda pixel, lsb: (pixel[0], pixel[1], apply_lsb(pixel[2], lsb)))
+    }
+    return pixel_lsb_mappers[channel]
+
+
+class SteganoParams :
+
+    def __init__(self) :
+        self.channel: Literal['r', 'g', 'b'] = 'r'
+        self.whiteboard: bool = False
+        self.background: int = 0
 
 
 class SteganoCallback :
 
-    def __init__(self, img: Image.Image, width: int, height: int, channel: Literal['r', 'g', 'b'], pbar: tqdm) :
+    def __init__(self, img: Image.Image, width: int, height: int, params: SteganoParams, pbar: tqdm) :
         self.pixels = img.load()
         self.x_offset = (img.width  - width)  // 2
         self.y_offset = (img.height - height) // 2
-        self.pixel_mapper = pixel_lsb_mappers[channel]
+        self.pixel_mapper = get_lsb_mapper(params.channel, params.whiteboard)
         self.pbar = pbar
         for x in range(0, self.x_offset) :
             for y in range(0, img.height) :
@@ -67,28 +90,36 @@ class SteganoCallback :
 
 def main() :
 
-    if len(sys.argv) > 1 and sys.argv[0] in ['-h', '--help'] :
+    args = deque(sys.argv[1:])
+    params = SteganoParams()
+
+    if len(args) > 1 and args[0] in ['-h', '--help'] :
         help()
         sys.exit(0)
-    
-    if len(sys.argv) < 5 :
+
+    if len(args) >= 1 and args[0] in ['--whiteboard', '--wb'] :
+        params.whiteboard = True
+        args.popleft()
+
+    if len(args) < 4 :
         help()
         sys.exit(1)
     
-    src_image_path = sys.argv[1]
-    dst_image_path = sys.argv[4]
-    stegano_image_path = sys.argv[2]
+    src_image_path = args[0]
+    dst_image_path = args[3]
+    stegano_image_path = args[1]
 
-    stegano_channel = sys.argv[3].lower()
+    stegano_channel = args[2].lower()
     if stegano_channel not in ['r', 'g', 'b'] :
         print(f"invalid channel: {stegano_channel} (should be one of 'r', 'g' or 'b')")
         sys.exit(1)
+    params.channel = stegano_channel
 
     src_img = Image.open(src_image_path)
     stegano_img = constrain_max_size(Image.open(stegano_image_path), src_img.width, src_img.height)
 
-    with tqdm(total = src_img.width*src_img.height) as pbar :
-        callback = SteganoCallback(src_img, stegano_img.width, stegano_img.height, stegano_channel, pbar)
+    with tqdm(total = src_img.width*src_img.height, unit = 'px') as pbar :
+        callback = SteganoCallback(src_img, stegano_img.width, stegano_img.height, params, pbar)
         floyd_steinberg(stegano_img, callback)
     src_img.save(dst_image_path)
 
